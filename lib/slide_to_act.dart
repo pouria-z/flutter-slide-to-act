a0625116c85984119c681b2e8d5bc02d08a43382
pouriazeinalzadeh <pouria.zeinalzadeh@gmail.com>
2022-08-17 21:24:08 +0430

    on tap animation

--- lib/slide_to_act.dart
+++ lib/slide_to_act.dart
@@ -225,8 +225,8 @@ class SlideActionState extends State<SlideAction> with TickerProviderStateMixin 
                                     // });
                                     // _cancelAnimation();
                                     _animationController
-                                        .reverse()
-                                        .whenComplete(() => _animationController.forward());
+                                        .reverse();
+                                        // .whenComplete(() => _animationController.forward());
                                     if (widget.onTap != null) {
                                       widget.onTap!.call();
                                     }
